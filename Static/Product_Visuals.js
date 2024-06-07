ab0a2d3643a093098648db7f0bc679ce6395c87e
lleiva25 <ljl.leiva@gmail.com>
2024-06-07 10:30:17 -0700

    Updating JSON file orientation

--- Static/Product_Visuals.js
+++ Static/Product_Visuals.js
@@ -1,10 +1,113 @@
-console.log("Hello world");
+//console.log("Hello world");
 
 //Refer to URL. Future Proof: Do not reference local files may cause eror
-let url = 'https://raw.githubusercontent.com/lleiva25/Medicaid-Drug-Rebate-Program/main/Output/Product_Data_2023';
+let url = 'https://raw.githubusercontent.com/lleiva25/Medicaid-Drug-Rebate-Program/main/Jupyter%20Output/Product_Data_2023';
+//var json = null;
+//Shows url link not data
+//console.log(url)
 
-fetch(url)
-.then(res => res.json())
-.then(out =>
-  console.log('Checkout this JSON!', out))
-.catch(err => { throw err });
+// Fetch the JSON data and console log it
+d3.json(url).then((data) => {
+           console.log(data)
+    // Populate the dropdown menu
+     var select = d3.select("#selDataset");
+     data.product_name.forEach((product) => {
+         select.append("option").text(product_name).property("value", product_name);
+     })
+    })
+    // // Add event listener for the dropdown menu change
+    // select.on("change", function () {
+    //     var newProduct = d3.select(this).property("value");
+    //     optionChanged(newProduct);
+    // });
+
+
+
+//     // Initialize the page with the first sample
+//     var firstProduct = data.product_name[0];
+//     //buildCharts(firstProduct);
+//     //buildMetadata(firstProduct);
+//     // Initialize the page with the first sample for gauge chart
+//     //buildGaugeChart(firstProduct);
+// });
+
+/////////////////////////////////////////////////
+// function buildMetadata(sample) {
+//     d3.json(url).then((data) => {
+//         var metadata = data.metadata;
+//         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+//         var result = resultArray[0];
+//         var PANEL = d3.select("#sample-metadata");
+
+//         // Clear any existing metadata
+//         PANEL.html("");
+
+//         // Add each key-value pair to the panel
+//         Object.entries(result).forEach(([key, value]) => {
+//             PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+//         });
+//     });
+// }
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+// function BarChart() {
+//     d3.json(url).then((data) => {
+//         var trace1 = {
+//             x: [],
+//             y: [],
+//             name: 'Medicaid Reimbursed',
+//             orientation: 'h',
+//             marker: {
+//                 color: 'rgba(55,128,191,0.6)',
+//                 width: 1
+//             },
+//             type: 'bar'
+//         };
+
+//         //Non-Medicaid portion of total Reimbursed
+//         var trace2 = {
+//             x: [],
+//             y: [],
+//             name: 'Non-Medicaid Reimbursed',
+//             orientation: 'h',
+//             type: 'bar',
+//             marker: {
+//                 color: 'rgba(255,153,51,0.6)',
+//                 width: 1
+//             }
+//         };
+
+
+//         var data = [trace1, trace2];
+
+//         //Add data into the graphs
+//         data.forEach(function (val) {
+//             trace1.x.push(val["product_name"]);
+//             trace1.y.push(val["Medicaid Amount Reimbursed"]);
+//             trace2.x.push(val["product_name"]);
+//             trace2.y.push(val["Non-medicaid Amount Reimbursed"]);
+//         });
+
+
+//         var layout = {
+//             title: 'Colored Bar Chart',
+//             barmode: 'stack'
+//         };
+
+//         Plotly.newPlot('sample-metadata', data, layout)}
+
+
+/////////////////////////////////////////////////
+// Function to handle changes in the dropdown selection
+function optionChanged(newProduct) {
+    console.log("New sample selected:", newProduct);
+    // buildCharts(newProduct);
+    buildMetadata(newProduct);
+    // buildGaugeChart(newProduct); 
+}
